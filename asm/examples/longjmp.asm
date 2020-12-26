77845ad3557aab005c52414aa5e78ec67fe3072e
pdaxrom <sashz@pdaxrom.org>
2020-12-27 00:27:35 +0400

    long jump and jsr (both use lr register)

---
+++ asm/examples/longjmp.asm
@@ -0,0 +1,13 @@
+	include ../include/pseudo.inc
+	include ../include/devmap.inc
+
+	org	$2000
+
+start	set	v1, 4
+loop	set	v0, hello
+	jsr	VEC_PUTSTR
+	sub	v1, v1, 1
+	bne	loop, v1, 0
+	jmp	VEC_RESET
+
+hello	db	10, 13, 'Hello, World!', 10, 13, 0
