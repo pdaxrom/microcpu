1559d992a21b967f8a548741aa732f932214d33f
pdaxrom <sashz@pdaxrom.org>
2021-02-24 18:17:27 +0400

    display text

--- asm/examples/longjmp.asm
+++ asm/examples/longjmp.asm
@@ -3,11 +3,18 @@
 
 	org	$2000
 
-start	set	v1, 4
+start	set	v0, text
+	jsr	VEC_DISPTXT
+	set	v1, 4
 loop	set	v0, hello
 	jsr	VEC_PUTSTR
 	sub	v1, v1, 1
 	bne	loop, v1, 0
-	jmp	VEC_RESET
+	jmp	VEC_WARMUP
 
 hello	db	10, 13, 'Hello, World!', 10, 13, 0
+
+text	db	"Long jum"
+	db	"p demo! "
+	db	"        "
+	db	"        "
